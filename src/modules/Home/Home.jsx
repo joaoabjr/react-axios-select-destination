ecc1b61fbb5cfed069def9072eca96de73ac850f
João Almeida <58539470+joaoabjr@users.noreply.github.com>
2022-10-30 23:13:40 -0300

    feat: add default values

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,44 +1,66 @@
 import React from "react";
 import { Destinations, PersonalInformation } from "../../components/context";
 import { Button } from "../../components/structure";
-import { Container, Input } from "reactstrap";
+import { Container } from "reactstrap";
 import styled from "styled-components";
 import { useForm, FormProvider } from "react-hook-form";
 
 const BoxInformation = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    column-gap: 96px;
-    align-items: center;
-    justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 96px;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Footer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 function Home() {
-    const methods = useForm({reValidateMode: "onSubmit"});
-    const { handleSubmit } = methods;
-    const onSubmit = (data) => console.log(data);
+  const methods = useForm({
+    reValidateMode: "onSubmit",
+    defaultValues: {
+      inputPhone: "",
+      inputCpf: "",
+      inputName: "",
+      inputEmail: "",
+    },
+  });
+  const { handleSubmit, resetField } = methods;
 
-    return (
-        <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit())}>
-                <Container>
-                    <BoxInformation>
-                        <PersonalInformation />
-                        <Destinations />
-                    </BoxInformation>
-                    <Footer>
-                        <Button type="submit" title="Entrar" />
-                    </Footer>
-                </Container>
-            </form>
-        </FormProvider>
-    );
+  const onSubmit = (data) => {
+    console.log("Envio dos dados: ", data);
+    alert("Dados enviados!");
+    resetField("inputName");
+    resetField("inputEmail");
+    resetField("inputPhone");
+    resetField("inputCpf");
+    resetField("inputCountry", {
+      defaultValue: null,
+    });
+    resetField("inputCity", {
+      defaultValue: null,
+    });
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container>
+          <BoxInformation>
+            <PersonalInformation />
+            <Destinations />
+          </BoxInformation>
+          <Footer>
+            <Button type="submit" title="Entrar" />
+          </Footer>
+        </Container>
+      </form>
+    </FormProvider>
+  );
 }
 
 export default Home;
